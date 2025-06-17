f537a1c2fe6755e61e9e0a57a537d1292a29e219
Mero <gjn_2012@foxmail.com>
2025-06-17 16:47:05 +0800

    feat: UI与页面样式补全

--- src/views/components/installer/steps/DiskPartition.vue
+++ src/views/components/installer/steps/DiskPartition.vue
@@ -1,108 +1,109 @@
 <template>
   <div class="disk-partition">
-    <h2>{{ t('install.disk_setup') }}</h2>
-    
-    <el-form :model="form" label-width="200px">
-      <el-form-item :label="t('install.target_disk')">
-        <el-select v-model="form.disk" placeholder="Select disk">
-          <el-option
-            v-for="disk in disks"
-            :key="disk"
-            :label="disk"
-            :value="disk"
-          />
-        </el-select>
-      </el-form-item>
-      
-      <el-divider />
-      
-      <div v-for="(part, index) in form.partitions" :key="index">
-        <h3>{{ t('install.partition') }} {{ index + 1 }}</h3>
-        
-        <el-form-item :label="t('install.size')">
-          <el-input v-model="part.size" placeholder="e.g. 10G, 100%">
-            <template #append>
-              <el-select v-model="part.unit" style="width: 80px">
-                <el-option label="MB" value="M" />
-                <el-option label="GB" value="G" />
-                <el-option label="%" value="%" />
-              </el-select>
-            </template>
-          </el-input>
-        </el-form-item>
-        
-        <el-form-item :label="t('install.mount_point')">
-          <el-input v-model="part.mountPoint" placeholder="e.g. /, /home" />
-        </el-form-item>
-        
-        <el-form-item :label="t('install.filesystem')">
-          <el-select v-model="part.fsType" placeholder="Select filesystem">
-            <el-option label="ext4" value="ext4" />
-            <el-option label="xfs" value="xfs" />
-            <el-option label="btrfs" value="btrfs" />
+    <div class="disk-partition-hr" />
+    <div class="disk-partition-bar">
+      <StepBar :step-num="2" />
+    </div>
+    <div class="disk-partition-form">
+      <el-form :model="form" label-width="auto" label-position="left">
+        <el-form-item prop="disk" :label="t('install.target_disk')">
+          <el-select v-model="form.disk" placeholder="Select disk">
+            <el-option
+              v-for="disk in disks"
+              :key="disk"
+              :label="disk"
+              :value="disk"
+            />
           </el-select>
         </el-form-item>
-        
-        <el-button
-          type="danger"
-          @click="removePartition(index)"
-          v-if="form.partitions.length > 1"
-        >
-          {{ t('common.remove') }}
-        </el-button>
-      </div>
-      
-      <el-button type="primary" @click="addPartition">
-        {{ t('install.add_partition') }}
-      </el-button>
-    </el-form>
+        <el-form-item prop="installType" :label="'安装方式'">
+          <el-select v-model="form.installType">
+            <el-option
+              v-for="item in installTypes"
+              :key="item.key"
+              :label="t(item.i18nKey)"
+              :value="item.key"
+            />
+          </el-select>
+        </el-form-item>
+        <el-form-item prop="partitionType" :label="'分区方式'">
+          <el-select v-model="form.partitionType">
+            <el-option label="自动分区" value="auto" />
+            <el-option label="手动分区" value="manual" disabled />
+          </el-select>
+        </el-form-item>
+      </el-form>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
+import { inject, reactive, ref } from 'vue'
 import { useI18n } from 'vue-i18n'
+import StepBar from '@/views/components/installer/comp/StepBar.vue'
+import { INSTALL_TYPES, INSTALL_INFO_KEY } from "@/utils/constant"
 
 const { t } = useI18n()
 
 const disks = ref(['/dev/sda', '/dev/sdb', '/dev/nvme0n1'])
+const installTypes = [...INSTALL_TYPES.entries()].map(v => ({ key: v[0], i18nKey: v[1] }))
 
-const form = ref({
+const form = reactive({
   disk: '',
-  partitions: [{
-    size: '',
-    unit: 'G',
-    mountPoint: '/',
-    fsType: 'ext4'
-  }]
+  installType: 'min',
+  partitionType: 'auto'
 })
 
-function addPartition() {
-  form.value.partitions.push({
-    size: '',
-    unit: 'G',
-    mountPoint: '',
-    fsType: 'ext4'
-  })
+const installInfo = inject(INSTALL_INFO_KEY, reactive({}))
+async function checkValid() {
+  // 待添加表单验证
+  installInfo.disk = form.disk
+  installInfo.installType = form.installType
+  installInfo.partitionType = form.partitionType
+  return true
 }
 
-function removePartition(index: number) {
-  form.value.partitions.splice(index, 1)
-}
+defineExpose({
+  checkValid
+})
 </script>
 
-<style scoped>
+<style scoped lang="scss">
 .disk-partition {
-  padding: 20px;
+  width: 100%;
+  height: 100%;
+  padding: 56px 0 72px;
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  flex-direction: column;
+
+  &-hr {
+    width: 100%;
+    height: 1px;
+    position: absolute;
+    top: 56px;
+    left: 0;
+    background-color: #dfe5ef;
+  }
+
+  &-bar {
+    width: calc(100% - 32px);
+    margin: 24px 0;
+  }
+
+  &-form {
+    width: calc(100% - 32px);
+  }
 }
 
-h2 {
-  margin-bottom: 20px;
-  color: #333;
-}
-
-h3 {
-  margin: 15px 0;
-  color: #666;
+.disk-partition-form {
+  :deep(.el-form-item__content) {
+    max-width: 320px;
+  }
+  :deep(.el-select) {
+    width: 100%;
+  }
 }
 </style>
